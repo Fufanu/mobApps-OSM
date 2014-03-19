a0ee443955f798b1eaa4a744171889f3c1a6bd60
Royalopino <Jan@erichsen-web.de>
2014-03-19 11:30:40 +0100

    So:
    Die Fragments können genutzt werden.
    
    !!! NEU !!!
    Ich habe eine Klasse "DataManager" erstellt um die *.dpx Dateien zu
    verwalten.
    
    Verwendung:
    	/* Konstruktor */
    	public DataManager()
    	
    	/* Checks if external storage is available for read and write */
    	public boolean isExternalStorageWritable() 
    
    	/* Checks if external storage is available to at least read */
    	public boolean isExternalStorageReadable() 
    
    	// Erzeugt das Verzeichniss auf der SD-Karte
    	private boolean createDirOnSD(String path)
    	
    	// Gibt eine Liste der Dateien zurück
    	public ArrayList<String> getAllFiles()
    
    	// Löscht die Datei
    	public boolean deleteGPSLogFile(String FileName)
    	
    	// Schreibt den String in die Angegebene Datei
    	public boolean writeToGPSLogFile(String FileName,String Value)
    
    	// Gibt den Inhalt der Datei als String zurück
    	public String readGPSLogFile(String FileName)

---
+++ src/fhfl/android/mobapps_osm/DataManager.java
@@ -0,0 +1,135 @@
+package fhfl.android.mobapps_osm;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.os.Environment;
+
+public class DataManager {
+	
+	SimpleDateFormat s = new SimpleDateFormat("dd-MM-yyyy_hh-mm-ss");
+	
+	//Konstanten
+	private String FOLDER_NAME = "/GPS_LOGS";
+	
+	/* Konstruktor */
+	public DataManager(){
+		createDirOnSD(FOLDER_NAME);
+	}
+	
+	/* Checks if external storage is available for read and write */
+	public boolean isExternalStorageWritable() {
+	    String state = Environment.getExternalStorageState();
+	    if (Environment.MEDIA_MOUNTED.equals(state)) {
+	        return true;
+	    }
+	    return false;
+	}
+
+	/* Checks if external storage is available to at least read */
+	public boolean isExternalStorageReadable() {
+	    String state = Environment.getExternalStorageState();
+	    if (Environment.MEDIA_MOUNTED.equals(state) ||
+	        Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+	        return true;
+	    }
+	    return false;
+	}
+
+	// Erzeugt das Verzeichniss auf der SD-Karte
+	private boolean createDirOnSD(String path){
+		File Folder = new File(Environment.getExternalStorageDirectory() + path);
+		
+		if(Folder.exists())
+		{
+			return false;
+		}
+		else
+		{
+			if(Folder.mkdir())
+				return true;
+			else
+				return false;
+		}
+	}
+	
+	// Erzeugt eine neue Datei und gibt diese zurück
+	public File createNewGPSLogFile(){
+		File file = new File(Environment.getExternalStorageDirectory() + "/GPS_LOGS/" , s.format(new Date()) + ".gpx" );
+		try {
+			file.createNewFile();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return file;
+	}
+
+	// Gibt eine Liste der Dateien zurück
+	public ArrayList<String> getAllFiles(){
+		ArrayList<String> Files = new ArrayList<String>();
+		
+		// get all the files from a directory
+		File[] fList = new File(Environment.getExternalStorageDirectory() + FOLDER_NAME).listFiles();
+		for (File f : fList) {
+			if(!f.isDirectory())
+				Files.add(f.getName());
+		}
+		return Files;
+	}
+
+	// Löscht die Datei
+	public boolean deleteGPSLogFile(String FileName){
+		File file = new File(Environment.getExternalStorageDirectory() + FOLDER_NAME + "/" + FileName);
+		return file.delete();
+	}
+	
+	// Schreibt den String in die Angegebene Datei
+	public boolean writeToGPSLogFile(String FileName,String Value){
+		try {
+			BufferedWriter bw = new BufferedWriter(new FileWriter(Environment.getExternalStorageDirectory() + FOLDER_NAME + "/" + FileName, true));
+			
+			bw.write(Value);
+			bw.close();
+			return true;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	// Gibt den Inhalt der Datei als String zurück
+	public String readGPSLogFile(String FileName){
+   
+	    BufferedReader reader;
+		try {
+			reader = new BufferedReader(new InputStreamReader(new FileInputStream(new File(Environment.getExternalStorageDirectory() + FOLDER_NAME + "/" + FileName))));
+			StringBuilder sb = new StringBuilder();
+		    String line = null;
+		    while ((line = reader.readLine()) != null) {
+		      sb.append(line).append("\n");
+		    }
+		    reader.close();
+		    return sb.toString();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "ERROR";
+		}
+	    
+	}
+}
--- src/fhfl/android/mobapps_osm/fragment_stats.java
+++ src/fhfl/android/mobapps_osm/fragment_stats.java
@@ -1,38 +1,84 @@
 package fhfl.android.mobapps_osm;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import android.R.string;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-public class fragment_stats extends Fragment implements OnClickListener{
-	public TextView TV_displayText;
-	public Button b;
-	public EditText userInput;
+public class fragment_stats extends Fragment implements OnClickListener, OnItemSelectedListener{
+	private Button btn_deleteFile;
+	private Spinner DropDown;
+	private DataManager DM;
+	private TextView DisplayFile;
+	private View view;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) 
 	{	
-		View view = inflater.inflate(R.layout.fragment_stats, container, false);
-		TV_displayText = (TextView)view.findViewById(R.id.textView2);
-		userInput = (EditText)view.findViewById(R.id.editText1);
-		b = (Button) view.findViewById(R.id.button1);
-        b.setOnClickListener(this);
+		view = inflater.inflate(R.layout.fragment_stats, container, false);
+		DisplayFile = (TextView)view.findViewById(R.id.fStats_DisplayFileSource);
+		DropDown = (Spinner)view.findViewById(R.id.fStats_DropDown_Files);
+		DropDown.setOnItemSelectedListener(this);
+		//userInput = (EditText)view.findViewById(R.id.editText1);
+		btn_deleteFile = (Button) view.findViewById(R.id.fStats_Button_DeleteFile);
+		btn_deleteFile.setOnClickListener(this);
+		
+		DM = new DataManager();
+		//DM.createNewGPSLogFile();
+		
+		refreshDropDown();
+		DisplayFile.setText("Startup");
+	
 		return view;
 	}
-	
-	
+
 	@Override
     public void onClick(View v) {
-        switch (v.getId()) {
-        case R.id.button1:
-        	TV_displayText.setText(userInput.getText());
-            break;
+        if (v.getId() ==  R.id.fStats_Button_DeleteFile)  {
+        	
+        	DM.deleteGPSLogFile(DropDown.getSelectedItem().toString());
+        	refreshDropDown();
+        	
         }
 	}
+	
+	public void refreshDropDown(){
+		ArrayList<String> Files = DM.getAllFiles();
+		
+		Collections.sort(Files, Collections.reverseOrder());		
+		
+		ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(view.getContext(),   android.R.layout.simple_spinner_item, Files);
+		spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // The drop down view
+		DropDown.setAdapter(spinnerArrayAdapter);
+	}
+
+	
+	@Override
+	public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
+			long arg3) {
+		// TODO Auto-generated method stub
+		DisplayFile.setText(DM.readGPSLogFile(DropDown.getSelectedItem().toString()));
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
 }
