f7b7ad981c07c3f98badb4334ff835ac53f74c1f
fufanu <philip@philiphermes.de>
2014-03-20 23:58:38 +0100

    fragment_map.java: Map wird dargestellt, Scale wird oben links dargestellt activity_main.xml: Padding zu allen Seiten entfernt damit Karte im Fullscreen; Am besten pro Fragment einstellen

--- src/fhfl/android/mobapps_osm/fragment_map.java
+++ src/fhfl/android/mobapps_osm/fragment_map.java
@@ -4,38 +4,67 @@ import org.osmdroid.ResourceProxy;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.util.ResourceProxyImpl;
 import org.osmdroid.views.MapView;
-import org.osmdroid.views.util.constants.MapViewConstants;
+import org.osmdroid.views.overlay.ScaleBarOverlay;
 
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class fragment_map extends Fragment implements MapViewConstants {
+public class fragment_map extends Fragment {
 	
-	private MapView mapView;
+	private DataManager DM;
+	private Context context;
 	private ResourceProxy mapResourceProxy;
+	private MapView mapView;
+	private ScaleBarOverlay mapScaleBarOverlay;
+	
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		
 		//Initialisierung
+		context = inflater.getContext();
+		DM = new DataManager();
+		View view = inflater.inflate(R.layout.fragment_map, container, false);
 		
-		// OSM einbinden
-		//mapView = new MapView(getActivity(), 256);
-		//mapView = (MapView) mapView.findViewById(R.id.mapView);
+		mapViewInit(view);
 		
-		mapResourceProxy = new ResourceProxyImpl(inflater.getContext().getApplicationContext());
-        mapView = new MapView(inflater.getContext(), 256, mapResourceProxy);
+		return view;
+	}
+	
+	private void mapViewInit(View view)
+	{
+		// OSM Map anlegen
+        mapView = (MapView) view.findViewById(R.id.mapView);
+        mapResourceProxy = new ResourceProxyImpl(context.getApplicationContext());
 		
-		mapView.setUseDataConnection(false);
-		mapView.getController().setZoom(14);
+		//Settings
+		mapView.getController().setZoom(15);
+		mapView.getController().setCenter(new GeoPoint(54.775235, 9.452794)); //Koordinaten der Mensa     
+		mapView.setUseSafeCanvas(true);
+		mapView.setClickable(false);
+		mapView.setBuiltInZoomControls(true);
 		mapView.setMultiTouchControls(true);
-		mapView.getController().setCenter(new GeoPoint(54.775235, 9.452794));
+		mapView.setUseDataConnection(false);
 		
-		return mapView;
-		//return inflater.inflate(R.layout.fragment_map, container, false);
+		// ScaleBar Overlay
+		mapScaleBarOverlay = new ScaleBarOverlay(context);
+		mapScaleBarOverlay.setLineWidth(2);
+		mapScaleBarOverlay.setTextSize(20);
+		
+        //Overlays zur Karte hinzufŸgen
+        mapView.getOverlays().add(mapScaleBarOverlay);
+        
+        //einzige brauchbare Lšsung um die Scalebar anzuzeigen. Nochmal nach einer schšneren Lšsung suchen
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
+    	    mapView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        
+        Log.d("OUTPUT", mapView.getOverlays().toString());
 	}
 }
