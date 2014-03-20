cda0deff1c00ceb3ee9c750b3b86ff67f4792059
fufanu <philip@philiphermes.de>
2014-03-20 14:12:50 +0100

    osmdroid eingebunden, karte wird noch nicht dargestellt

--- README.md
+++ README.md
@@ -6,18 +6,5 @@ mobApps HA13 Android OSM
 
 Android APP as OSM viewer with track saving feature
 
-Raul Boettcher
-
-
-Hausarbeit
 
 OSM-GPS-Tracer
-
-Anas
-
-Vollhorsterei COKG was here....
-
-Ihr seid alle Penner!
-
-
-Nochmal
--- src/fhfl/android/mobapps_osm/fragment_map.java
+++ src/fhfl/android/mobapps_osm/fragment_map.java
@@ -1,15 +1,41 @@
 package fhfl.android.mobapps_osm;
 
+import org.osmdroid.ResourceProxy;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.util.ResourceProxyImpl;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.util.constants.MapViewConstants;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class fragment_map extends Fragment {
+public class fragment_map extends Fragment implements MapViewConstants {
+	
+	private MapView mapView;
+	private ResourceProxy mapResourceProxy;
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.fragment_map, container, false);
+		
+		//Initialisierung
+		
+		// OSM einbinden
+		//mapView = new MapView(getActivity(), 256);
+		//mapView = (MapView) mapView.findViewById(R.id.mapView);
+		
+		mapResourceProxy = new ResourceProxyImpl(inflater.getContext().getApplicationContext());
+        mapView = new MapView(inflater.getContext(), 256, mapResourceProxy);
+		
+		mapView.setUseDataConnection(false);
+		mapView.getController().setZoom(14);
+		mapView.setMultiTouchControls(true);
+		mapView.getController().setCenter(new GeoPoint(54.775235, 9.452794));
+		
+		return mapView;
+		//return inflater.inflate(R.layout.fragment_map, container, false);
 	}
 }
